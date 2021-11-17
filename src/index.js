0df84172234b3d74efb80928023f3d5e7d0f6648
TristinCodingham <tristinc.codingham@gmail.com>
2021-11-17 13:45:19 +0200

    Update: Parallax -> Fixed scroll

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 import './assets/css/index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
-    <ParallaxProvider>
       <App />
-    </ParallaxProvider>
   </BrowserRouter>,
   
   document.getElementById('root')
